fe7047728c7d8e4e2d6319ad4c0706914bd1ed18
Justin S <Ownasaurus@gmail.com>
2023-06-17 15:04:59 -0400

    improve behavior of X'ing out of window; phantom process less likely

--- Source/Project64/UserInterface/MainWindow.cpp
+++ Source/Project64/UserInterface/MainWindow.cpp
@@ -1086,6 +1086,15 @@ LRESULT CALLBACK CMainGui::MainGui_Proc(HWND hWnd, DWORD uMsg, DWORD wParam, DWO
             }
         }
         break;
+    case WM_CLOSE:
+        if (ck->isPlayingKailleraGame)
+        {
+            ck->endGame();
+            ck->isPlayingKailleraGame = false;
+            g_BaseSystem->CloseSystem();
+            break;
+        }
+        // otherwise PURPOSELY fall through to the WM_DESTROY case
     case WM_DESTROY:
         WriteTrace(TraceUserInterface, TraceDebug, "WM_DESTROY - start");
         {
